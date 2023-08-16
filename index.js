85e878e5fd4a4207ec91f000c2fb03a0628e3503
Eduardogrq <eduardo.grquinonez@gmail.com>
2023-08-15 23:58:58 -0600

    wip: adding mysql2 to connect to mysql

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from "express";
+import {pool} from "./db.js";
 
 const app = express();
+
+app.get('/ping', async(req, res) => {
+    const result = await pool.query("SELECT 1 + 1 AS solution");
+    return res.json(result[0]);
+})
 
 app.get("/employees", (req, res) => {
     res.send("Obteniendo empleados");
