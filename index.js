01505c2dbb3b684cd3997c2e1ac7a6e97b13d399
Eduardogrq <eduardo.grquinonez@gmail.com>
2023-08-15 23:28:40 -0600

    wip: adding nodemon package and creating first employees routes

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hello World')
+import express from "express";
+
+const app = express();
+
+app.get("/employees", (req, res) => {
+    res.send("Obteniendo empleados");
+});
+
+app.post("/employees", (req, res) => {
+    res.send("Creando empleado");
+});
+
+app.put("/employees/:id", (req, res) => {
+    res.send("Actualizando empleado");
+});
+
+app.delete("/employees/:id", (req, res) => {
+    res.send("Eliminando empleado");
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+
